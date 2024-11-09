a97d64b03fa4386b0414c6dbaed072e8cc2e3274
mxyhi <imxyi@hotmail.com>
2024-11-09 15:10:03 +0800

    feat(hello_ic_backend): add get_submitted_names endpoint and update dependencies

--- src/hello_ic_backend/src/lib.rs
+++ src/hello_ic_backend/src/lib.rs
@@ -1,4 +1,17 @@
-#[ic_cdk::query]
+use ic_cdk_macros::{query, update};
+use std::cell::RefCell;
+
+thread_local! {
+    static SUBMITTED_NAMES: RefCell<Vec<String>> = RefCell::new(Vec::new());
+}
+
+#[update]
 fn greet(name: String) -> String {
+    SUBMITTED_NAMES.with(|names| names.borrow_mut().push(name.clone()));
     format!("Hello, {}!", name)
+}
+
+#[query]
+fn get_submitted_names() -> Vec<String> {
+    SUBMITTED_NAMES.with(|names| names.borrow().clone())
 }
--- src/hello_ic_frontend/src/App.jsx
+++ src/hello_ic_frontend/src/App.jsx
@@ -1,29 +1,37 @@
-import { useState } from 'react';
-import { hello_ic_backend } from 'declarations/hello_ic_backend';
+import { useState } from "react";
+import { hello_ic_backend } from "declarations/hello_ic_backend";
 
 function App() {
-  const [greeting, setGreeting] = useState('');
+  const [greeting, setGreeting] = useState("");
+  const [submittedNames, setSubmittedNames] = useState([]);
 
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
     const name = event.target.elements.name.value;
-    hello_ic_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
+    const greeting = await hello_ic_backend.greet(name);
+    setGreeting(greeting);
+    const submittedNames = await hello_ic_backend.get_submitted_names();
+    setSubmittedNames(submittedNames);
     return false;
   }
 
   return (
     <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
       <form action="#" onSubmit={handleSubmit}>
         <label htmlFor="name">Enter your name: &nbsp;</label>
         <input id="name" alt="Name" type="text" />
         <button type="submit">Click Me!</button>
       </form>
       <section id="greeting">{greeting}</section>
+
+      <section id="submitted-names">
+        <h2>All Greeted Names</h2>
+        <ul>
+          {submittedNames.map((it, i) => {
+            return <li key={i}>{it}</li>;
+          })}
+        </ul>
+      </section>
     </main>
   );
 }
